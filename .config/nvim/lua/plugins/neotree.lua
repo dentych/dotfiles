1ae53be802417c0ddd18a0548c6f48f564a0b4a1
dentych <dennis@tychsen.me>
2024-04-17 09:45:11 +0200

    Do not hide dotfiles

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -18,6 +18,9 @@ return {
     },
 	opts = {
 		filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+            },
 			follow_current_file = {
 				enabled = true,
 			},
