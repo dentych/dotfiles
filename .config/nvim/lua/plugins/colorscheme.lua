0e198fa94a228265bc2938ea7dc3d4bfa67c2ef9
dentych <dennis@tychsen.me>
2024-05-01 15:25:07 +0200

    Update comment color to lighgray

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,16 @@
 return {
-    {
-        "morhetz/gruvbox",
-        priority = 1000,
-    },
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "VeryLazy",
-        opts = {},
-    }
+	{
+		"morhetz/gruvbox",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("gruvbox")
+
+			vim.api.nvim_set_hl(0, "Comment", { fg = "lightgray" })
+		end,
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
 }
