dc1506d0e9806cc6197c4f9bd8db3449487de24d
dentych <dennis@tychsen.me>
2024-05-16 18:10:20 +0200

    Switch back to gruvbox, but with hard contrast

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,13 @@
 return {
 	{
-		"morhetz/gruvbox",
+		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
+		opts = {
+			contrast = "hard",
+		},
 		config = function()
-			--vim.cmd.colorscheme("gruvbox")
-
-			vim.api.nvim_set_hl(0, "Comment", { link = "GruvboxFg0" })
-			-- Write same highlight methods as the three above, but without the hl- prefix
-			vim.api.nvim_set_hl(0, "LspReferenceText", { link = "GruvboxFg0" })
-			vim.api.nvim_set_hl(0, "LspReferenceRead", { link = "LspReferenceText" })
-			vim.api.nvim_set_hl(0, "LspReferenceWrite", { link = "LspReferenceText" })
+			require("gruvbox").setup(opts)
+			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 	{
@@ -21,7 +19,7 @@ return {
 		},
 		config = function(_, opts)
 			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
+			--vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 	{
