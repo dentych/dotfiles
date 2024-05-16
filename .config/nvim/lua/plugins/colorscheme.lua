4a089bd70c3542fb773d7cfae050c975ed7c519b
dentych <dennis@tychsen.me>
2024-05-16 18:02:00 +0200

    Try out catppuccin color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,13 +3,25 @@ return {
 		"morhetz/gruvbox",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("gruvbox")
+			--vim.cmd.colorscheme("gruvbox")
 
 			vim.api.nvim_set_hl(0, "Comment", { link = "GruvboxFg0" })
 			-- Write same highlight methods as the three above, but without the hl- prefix
 			vim.api.nvim_set_hl(0, "LspReferenceText", { link = "GruvboxFg0" })
 			vim.api.nvim_set_hl(0, "LspReferenceRead", { link = "LspReferenceText" })
 			vim.api.nvim_set_hl(0, "LspReferenceWrite", { link = "LspReferenceText" })
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 	{
