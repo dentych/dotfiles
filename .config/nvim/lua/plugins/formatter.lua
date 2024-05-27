067535724c4b71fe126afb15b7e08b77811cfd78
dentych <dennis@tychsen.me>
2024-05-27 10:15:15 +0200

    Add graphql prettierd

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,23 +1,30 @@
 return {
-    'stevearc/conform.nvim',
-    event = "BufEnter",
-    opts = {
-        formatters_by_ft = {
-            sql = { "sql_formatter" },
-            go = { "goimports" },
-            lua = { "stylua" },
-        },
-        formatters = {
-            sql_formatter = {
-                args = { "-l", "postgresql" },
-            },
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
-    },
-    keys = {
-        { "<leader>cf", function() require('conform').format({ lsp_fallback = true }) end, desc = "Format file" },
-    }
+	"stevearc/conform.nvim",
+	event = "BufEnter",
+	opts = {
+		formatters_by_ft = {
+			sql = { "sql_formatter" },
+			go = { "goimports" },
+			lua = { "stylua" },
+			graphql = { "prettierd" },
+		},
+		formatters = {
+			sql_formatter = {
+				args = { "-l", "postgresql" },
+			},
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ lsp_fallback = true })
+			end,
+			desc = "Format file",
+		},
+	},
 }
