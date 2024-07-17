1055fe07de427a6b3ae8f84c7285ad01758e18b1
dentych <dennis@tychsen.me>
2024-07-17 14:47:53 +0200

    Close neo-tree after opening a file

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -32,5 +32,13 @@ return {
 				["w"] = "none",
 			},
 		},
+		event_handlers = {
+			{
+				event = "file_open_requested",
+				handler = function()
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			},
+		},
 	},
 }
