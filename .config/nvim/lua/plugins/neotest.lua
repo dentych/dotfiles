d1937ab42550290ab72b035fd22b897dadd13eea
dentych <dennis@tychsen.me>
2024-07-31 13:16:18 +0200

    Debug stuff

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -20,6 +20,7 @@ return {
 		adapters = {
 			["neotest-golang"] = {
 				go_test_args = { "-tags=integration" },
+				testify_enabled = true,
 			},
 		},
 		status = { virtual_text = true },
@@ -36,7 +37,49 @@ return {
 			table.insert(adapters, require(name)(config))
 		end
 		opts.adapters = adapters
+
+		local neotest_ns = vim.api.nvim_create_namespace("neotest")
+		vim.diagnostic.config({
+			virtual_text = {
+				format = function(diagnostic)
+					-- Replace newline and tab characters with space for more compact diagnostics
+					local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+					return message
+				end,
+			},
+		}, neotest_ns)
 		require("neotest").setup(opts)
+
+		if require("util").has("trouble") then
+			opts.consumers = opts.consumers or {}
+			-- Refresh and auto close trouble after running tests
+			---@type neotest.Consumer
+			opts.consumers.trouble = function(client)
+				client.listeners.results = function(adapter_id, results, partial)
+					if partial then
+						return
+					end
+					local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+					local failed = 0
+					for pos_id, result in pairs(results) do
+						if result.status == "failed" and tree:get_key(pos_id) then
+							failed = failed + 1
+						end
+					end
+					vim.schedule(function()
+						local trouble = require("trouble")
+						if trouble.is_open() then
+							trouble.refresh()
+							if failed == 0 then
+								trouble.close()
+							end
+						end
+					end)
+					return {}
+				end
+			end
+		end
 	end,
     -- stylua: ignore
     keys = {
