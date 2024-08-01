9f4116ebf8112708d35b434f044ea7d542fa65b5
dentych <dennis@tychsen.me>
2024-08-01 13:18:15 +0200

    Formatting

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,15 +1,15 @@
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 	callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+		local bufnr = vim.api.nvim_get_current_buf()
+		local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
 
-        for _, client in ipairs(clients) do
-            if client.server_capabilities.documentHighlightProvider then
-                vim.lsp.buf.document_highlight()
-                return
-            end
-        end
-    end,
+		for _, client in ipairs(clients) do
+			if client.server_capabilities.documentHighlightProvider then
+				vim.lsp.buf.document_highlight()
+				return
+			end
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("CursorMoved", {
