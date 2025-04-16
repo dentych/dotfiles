1e4dca73294a5fffda63f4beeea5c1ca9e38a963
dentych <dennis@tychsen.me>
2025-04-16 13:28:29 +0200

    Set sql-formatter as sql formatting tool

--- .config/nvim/lua/plugins/override.lua
+++ .config/nvim/lua/plugins/override.lua
@@ -40,4 +40,12 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sql_formatter", "sqlfluff" },
+      },
+    },
+  },
 }
