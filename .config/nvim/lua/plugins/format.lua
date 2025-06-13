d2350e25a497095c18bb5919a5b1842a2809cf52
dentych <dennis@tychsen.me>
2025-06-13 13:14:28 +0200

    Set default sql language to postgresql in sql-formatter

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -5,6 +5,9 @@ return {
       opts.formatters.sqlfluff = nil
 
       opts.formatters_by_ft.sql = { "sql_formatter" }
+      opts.formatters.sql_formatter = {
+        prepend_args = { "-l", "postgresql" },
+      }
     end,
   },
   {
