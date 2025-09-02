46b0b03b9a8969b3c01ea37d4ba036b0c424d6c9
dentych <dennis@tychsen.me>
2025-09-02 13:04:58 +0200

    Remove AI copilot

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -3,11 +3,7 @@ return {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
-    opts = {
-      -- add any opts here
-      -- for example
-      provider = "copilot",
-    },
+    opts = {},
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
