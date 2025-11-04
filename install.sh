185457e5561c73ec8c4bb5085ef6e4540f49b0bc
dentych <dennis@tychsen.me>
2025-11-04 13:27:19 +0100

    Restructure dotfiles :)

---
+++ install.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+command_exist() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+if ! command_exist brew; then
+    echo 'Homebrew not found'
+    exit -1
+fi
+
+# Install brew packages
+echo 'Updating Homebrew...'
+brew update
+
+echo 'Installing packages...'
+brew bundle --file=./Brewfile
+
+# Symlink dotfiles
+echo 'Copying and linking dotfiles...'
+stow -v --no-folding aerospace fish foot hypr nvim waybar zellij git editorconfig
