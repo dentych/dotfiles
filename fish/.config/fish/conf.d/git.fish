6d85794e76f0afa2d53a5ca5b4d18b577d72b56e
dentych <dennis@tychsen.me>
2025-11-05 13:30:13 +0100

    Git switch to default branch now dynamic

--- fish/.config/fish/conf.d/git.fish
+++ fish/.config/fish/conf.d/git.fish
@@ -2,7 +2,15 @@ alias g='git'
 alias grs='git restore'
 alias gst='git status'
 alias gsw='git switch'
-alias gswm='git switch master'
+
+function gswm
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@')
+    if test -z "$default_branch"
+        set default_branch (git rev-parse --abbrev-ref HEAD)
+    end
+    git switch $default_branch
+end
+
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gf='git fetch'
@@ -17,7 +25,14 @@ alias gc!='git commit -v --amend'
 alias gca='git commit -v -a'
 alias gca!='git commit -v -a --amend'
 alias gcmsg='git commit -m'
-alias gcm='git switch master'
+
+function gcm
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@')
+    if test -z "$default_branch"
+        set default_branch (git rev-parse --abbrev-ref HEAD)
+    end
+    git switch $default_branch
+end
 alias ga='git add'
 alias gdca='git diff --cached'
 alias gdab='git branch | grep -v \'*\' | xargs git branch -D'
