3f2b4ba6db42de03b12891cb4f3d3b3daa66efaf
dentych <dennis@tychsen.me>
2025-11-11 11:25:15 +0100

    Remove luarc, enable tmux and update nvim lazy extras

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
 command_exist() {
-    command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 if ! command_exist brew; then
-    echo 'Homebrew not found'
-    exit -1
+	echo 'Homebrew not found'
+	exit -1
 fi
 
 # Install brew packages
@@ -18,4 +18,4 @@ brew bundle --file=./Brewfile
 
 # Symlink dotfiles
 echo 'Copying and linking dotfiles...'
-stow -v --no-folding aerospace fish foot ghostty hypr nvim waybar zellij git editorconfig
+stow -v --no-folding aerospace fish foot ghostty hypr nvim waybar zellij git editorconfig tmux
