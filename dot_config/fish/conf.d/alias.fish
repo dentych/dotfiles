2908cb35a12566a362915f9e27be1fd54bf2c238
dentych <dennis@tychsen.me>
2026-02-13 13:27:35 +0100

    feat(fish): add aliases for yazi and chezmoi

--- dot_config/fish/conf.d/alias.fish
+++ dot_config/fish/conf.d/alias.fish
@@ -3,6 +3,9 @@ if status is-interactive
     alias k kubectl
     alias gtc "cd ~/code"
 
+    alias yz yazi
+    alias c chezmoi
+
     # understory
     alias aws-ship-dev "node ../node-aws-env/bin/deploy.js -e development --profile understory-dev -g ../glue/development/glue.json"
     alias aws-ship-prod "node ../node-aws-env/bin/deploy.js -e production --profile understory-prod -g ../glue/production/glue.json"
