95ad088a185ab7bba0a2e4c4f1acceb94f596a83
dentych <dennis@tychsen.me>
2026-09-11 21:09:34 +0200

    fix(fish): use quotes to properly unfold the $HOME variable

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,7 +7,7 @@ if status is-interactive
     fish_add_path -g "$HOME/go/bin"
 
     # pnpm
-    set -gx PNPM_HOME '$HOME/.local/share/pnpm'
+    set -gx PNPM_HOME "$HOME/.local/share/pnpm"
     if not string match -q -- "$PNPM_HOME/bin" $PATH
         set -gx PATH "$PNPM_HOME/bin" $PATH
     end
